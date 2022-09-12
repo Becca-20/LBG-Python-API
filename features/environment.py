b6191ec27b9de05cc36b3f585c91821191a0e015
Becca-20 <cerulean.rebecca20@hotmail.com>
2022-09-13 00:03:06 +0100

    updated chrome driver path

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.browser = webdriver.Chrome(r"C:\Users\Admin\LBG-Python-API\webdrivers\chromedriver.exe")
+    context.browser = webdriver.Chrome(r"C:\Users\Admin\MEA\chromedriver.exe")
 
 def after_all(context):
     context.browser.quit()
